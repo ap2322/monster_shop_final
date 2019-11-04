c0de1666a48f4d49fbcc26c17ffe4a03af019209
Alice Post <alicep2322@gmail.com>
2019-11-04 13:40:37 -0700

    Add instance method to return addresses sorted by use

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe User do
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @user = create(:user)
+      @address_1 = create(:address, user_id: @user.id)
+      @address_2 = create(:address, user_id: @user.id, use: 2)
+      @address_3 = create(:address, user_id: @user.id, use: 1)
+    end
+    it 'should sort user addresses by use' do
+      expected = [@address_1, @address_3, @address_2]
+      expect(@user.addresses_by_use).to eq expected
+    end
+  end
 end
