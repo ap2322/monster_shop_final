fad3cee911e5aaac968e23418589cceb62bdd839
Alice Post <alicep2322@gmail.com>
2019-11-04 20:14:13 -0700

    Update tests and corresponding views with updated user, address, and order resources

--- spec/features/merchant/order_show_spec.rb
+++ spec/features/merchant/order_show_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe 'Merchant Order Show Page' do
       @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @merchant_2 = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @m_user = create(:user, :with_addresses, merchant_id: @merchant_1.id)
+      @m_address = @m_user.addresses.first
       @ogre = @merchant_1.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @merchant_1.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @merchant_2.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 1 )
       @order_1 = @m_user.orders.create!(address_id: @m_user.addresses.first.id, status: "pending")
-      @order_2 = @m_user.orders.create!(address_id: @m_user.addresses.first.id, status: "pending") 
+      @order_2 = @m_user.orders.create!(address_id: @m_user.addresses.first.id, status: "pending")
       @order_item_1 = @order_1.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2, fulfilled: false)
       @order_item_2 = @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2, fulfilled: true)
       @order_item_3 = @order_2.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2, fulfilled: false)
@@ -22,10 +23,10 @@ RSpec.describe 'Merchant Order Show Page' do
       visit "/merchant/orders/#{@order_2.id}"
 
       expect(page).to have_content(@m_user.name)
-      expect(page).to have_content(@m_user.address)
-      expect(page).to have_content(@m_user.city)
-      expect(page).to have_content(@m_user.state)
-      expect(page).to have_content(@m_user.zip)
+      expect(page).to have_content(@m_address.address)
+      expect(page).to have_content(@m_address.city)
+      expect(page).to have_content(@m_address.state)
+      expect(page).to have_content(@m_address.zip)
 
       within "#order-item-#{@order_item_3.id}" do
         expect(page).to have_link(@order_item_3.item.name)
