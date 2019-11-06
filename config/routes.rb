e8cdf70e8cbc367c8d5c0de3d210a3a17d251b54
Alice Post <alicep2322@gmail.com>
2019-11-05 21:57:48 -0700

    Add specific / route for home

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get :root, to: 'welcome#index'
 
+  get '/', to: 'welcome#index'
+  
   resources :merchants do
     resources :items, only: [:index]
   end
