a114cd9e55814c1b0134561cf76fc0c1d616c476
Alice Post <alicep2322@gmail.com>
2019-11-16 14:54:32 -0700

    Scope and resource profile, orders, addresses routes

--- config/routes.rb
+++ config/routes.rb
@@ -37,23 +37,33 @@ Rails.application.routes.draw do
 
   get '/registration', to: 'users#new', as: :registration
   resources :users, only: [:create, :update]
-  patch '/user/:id', to: 'users#update'
+  # patch '/user/:id', to: 'users#update'
+
   get '/profile', to: 'users#show'
   get '/profile/edit', to: 'users#edit'
   get '/profile/edit_password', to: 'users#edit_password'
-  get '/orders/new', to: 'user/orders#new'
-  post '/orders', to: 'user/orders#create'
-  get '/profile/orders', to: 'user/orders#index'
-  get '/profile/orders/:id', to: 'user/orders#show'
+
+  resources :orders, only: [:new, :create], module: :user
+  # get '/orders/new', to: 'user/orders#new'
+  # post '/orders', to: 'user/orders#create'
+
+  scope :profile, module: :user do
+    resources :orders, only: [:index, :show]
+  end
+  # get '/profile/orders', to: 'user/orders#index'
+  # get '/profile/orders/:id', to: 'user/orders#show'
   get '/profile/orders/:id/change_address', to: 'user/orders#change_address'
   post '/profile/orders/:id/change_address', to: 'user/orders#update_address'
   delete '/profile/orders/:id', to: 'user/orders#cancel'
 
-  get '/profile/addresses/new', to: 'user/addresses#new'
+  scope :profile, module: :user do
+    resources :addresses, except: [:show, :create]
+  end
+  # get '/profile/addresses/new', to: 'user/addresses#new'
   post '/profile/addresses/new', to: 'user/addresses#create'
-  get '/profile/addresses/:id/edit', to: 'user/addresses#edit'
-  patch '/profile/addresses/:id', to: 'user/addresses#update'
-  delete '/profile/addresses/:id', to: 'user/addresses#destroy'
+  # get '/profile/addresses/:id/edit', to: 'user/addresses#edit'
+  # patch '/profile/addresses/:id', to: 'user/addresses#update'
+  # delete '/profile/addresses/:id', to: 'user/addresses#destroy'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#login'
